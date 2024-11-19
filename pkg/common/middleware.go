fb3e55687a01d7692d1b882ea680e5acdb82dcc1
malfiefauzanakbar <muhamadalfiefauzanakbar@gmail.com>
2024-11-20 02:07:10 +0700

    temporary disable auth session

--- pkg/common/middleware.go
+++ pkg/common/middleware.go
@@ -1,20 +1,20 @@
 package common
 
 import (
-	"fmt"
-	"net/http"
-	"reseller-jh-be/base"
-	"reseller-jh-be/constant"
+	// "fmt"
+	// "net/http"
+	// "reseller-jh-be/base"
+	// "reseller-jh-be/constant"
 	// "strings"
-	"time"
+	// "time"
 
-	"github.com/gin-contrib/sessions"
+	// "github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		token := c.GetHeader("token")
+		// token := c.GetHeader("token")
 		// parts := strings.Split(authorization, " ")
 
 		// if len(parts) != 2 || parts[0] != "Bearer" {
@@ -24,44 +24,44 @@ func AuthMiddleware() gin.HandlerFunc {
 		// }
 
 		// token := parts[1]
-		decryptToken, err := Decrypt(token)
-		if err != nil {
-			base.RespondError(c, http.StatusUnauthorized, constant.Unauthorized, err)
-			c.Abort()
-			return
-		}
+		// decryptToken, err := Decrypt(token)
+		// if err != nil {
+		// 	base.RespondError(c, http.StatusUnauthorized, constant.Unauthorized, err)
+		// 	c.Abort()
+		// 	return
+		// }
 
-		session := sessions.Default(c)
-		if token == "" || decryptToken != session.Get("token") {
-			base.RespondError(c, http.StatusUnauthorized, constant.Unauthorized, nil)
-			c.Abort()
-			return
-		}
+		// session := sessions.Default(c)
+		// if token == "" || decryptToken != session.Get("token") {
+		// 	base.RespondError(c, http.StatusUnauthorized, constant.Unauthorized, nil)
+		// 	c.Abort()
+		// 	return
+		// }
 
-		now := time.Now()
-		dateTime := now.Format("2006-01-02 15:04:05")
-		if session.Get("lastAccessed") == nil {
-			session.Set("lastAccessed", dateTime)
-			session.Save()
-		} else {
-			localLocation := time.Now().Location()
-			lastAccessed, err := time.ParseInLocation("2006-01-02 15:04:05", fmt.Sprintf("%v", session.Get("lastAccessed")), localLocation)
-			if err != nil {
-				base.RespondError(c, http.StatusInternalServerError, constant.InternalServerError, err)
-				return
-			}
+		// now := time.Now()
+		// dateTime := now.Format("2006-01-02 15:04:05")
+		// if session.Get("lastAccessed") == nil {
+		// 	session.Set("lastAccessed", dateTime)
+		// 	session.Save()
+		// } else {
+		// 	localLocation := time.Now().Location()
+		// 	lastAccessed, err := time.ParseInLocation("2006-01-02 15:04:05", fmt.Sprintf("%v", session.Get("lastAccessed")), localLocation)
+		// 	if err != nil {
+		// 		base.RespondError(c, http.StatusInternalServerError, constant.InternalServerError, err)
+		// 		return
+		// 	}
 
-			duration := now.Sub(lastAccessed)
-			if duration >= time.Hour {
-				session.Clear()
-				base.RespondError(c, 440, constant.SessionExpired, nil)
-				c.Abort()
-				return
-			} else {
-				session.Set("lastAccessed", dateTime)
-				session.Save()
-			}
-		}
+		// 	duration := now.Sub(lastAccessed)
+		// 	if duration >= time.Hour {
+		// 		session.Clear()
+		// 		base.RespondError(c, 440, constant.SessionExpired, nil)
+		// 		c.Abort()
+		// 		return
+		// 	} else {
+		// 		session.Set("lastAccessed", dateTime)
+		// 		session.Save()
+		// 	}
+		// }
 
 		c.Next()
 	}
