9ef19781255ca821f636a1f1e2d1e00dd35199e6
malfiefauzanakbar <muhamadalfiefauzanakbar@gmail.com>
2024-11-21 23:12:58 +0700

    status_id

--- internal/reseller/repository/reseller_repository.go
+++ internal/reseller/repository/reseller_repository.go
@@ -41,8 +41,10 @@ func (r *ResellerRepository) GetAllReseller(statusID int64, reqReseller request.
 		query = query.Where("(lower(r.fullname) like ? OR lower(r.whatsapp_no) like ? OR lower(r.email) like ? OR r.nik like ? OR lower(r.address) like ?)", "%"+reqPagination.Keyword+"%", "%"+reqPagination.Keyword+"%", "%"+reqPagination.Keyword+"%", "%"+reqPagination.Keyword+"%", "%"+reqPagination.Keyword+"%")
 	}
 
-	if statusID > 0 {
+	if statusID == 1 {
 		query = query.Where("r.status_id = ? ", statusID)
+	}else{
+		query = query.Where("r.status_id >= 2 ")
 	}
 
 	if reqReseller.StartDate != "" && reqReseller.EndDate != "" {
