5faa7768d380f18df4d2e41029c183860bb0f222
muhamad.akbar <muhamad.akbar@hyperscal.com>
2024-12-06 16:32:55 +0700

    allow origin

--- internal/app.go
+++ internal/app.go
@@ -22,7 +22,7 @@ func NewApp(cfg *config.Config) *Application {
 	r.Static("/uploads", "./uploads")
 	r.Use(sessions.Sessions("session", store))
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://reseller.jimshoneyofficial.co.id"},
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "token"},
 		ExposeHeaders:    []string{"Content-Length"},
